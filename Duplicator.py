736da6431801dee4aa27cae9c73681a3e231c347
Roelf de Kock <kiemdoder@gmail.com>
2019-06-08 18:57:44 +0200

    duplications limit option, fixed rotation

--- Duplicator.py
+++ Duplicator.py
@@ -1,11 +1,11 @@
 import bpy
-from bpy.props import BoolProperty, FloatProperty
+from bpy.props import BoolProperty, FloatProperty, IntProperty
 
 from .dup_duplicate_cursor_op import DUP_DuplicateToCursorOperator
 from .dup_duplicate_faces_op import DUP_DuplicateToFacesOperator
 from .dup_duplicate_vertices_op import DUP_DuplicateToVerticesOperator
 from .dup_delete_duplicated_children_op import DUP_DeleteDuplicatedChildrenOperator
-from .dup_panel import DUP_PT_Panel
+from .dup_panel import DuplicationPanel, DuplicationOptionsPanel, DuplicationTargetPanel
 
 bpy.types.Scene.dup_duplicate_tree = BoolProperty(
     name="Duplicate tree",
@@ -13,31 +13,31 @@ bpy.types.Scene.dup_duplicate_tree = BoolProperty(
     default=True)
 
 bpy.types.Scene.dup_randomise_duplicated = BoolProperty(
-    name="Randomise rotation",
+    name="Rotation",
     description="Randomise the rotation of an object when it is duplicated",
     default=True)
 
 bpy.types.Scene.dup_random_rotation_factor = FloatProperty(
-    name="Random rotation",
+    name="Random rotation:",
     description="The amount of random rotation to be applied to duplicates",
     default=1.0,
     min=0.0,
     max=1.0)
 
 bpy.types.Scene.dup_randomise_displacement = BoolProperty(
-    name="Randomise displacement",
+    name="Displace",
     description="Randomise the displacement of an object when it is duplicated",
     default=True)
 
 bpy.types.Scene.dup_random_displacement = FloatProperty(
-    name="Random displacement",
+    name="Random displacement:",
     description="The amount of random displacement to be applied to duplicates",
     default=0.5,
     min=0.0,
     max=1.0)
 
 bpy.types.Scene.dup_randomise_scale = BoolProperty(
-    name="Randomise scale",
+    name="Scale",
     description="Randomise the scale of an object when it is duplicated",
     default=False)
 
@@ -55,12 +55,25 @@ bpy.types.Scene.dup_density = FloatProperty(
     min=-0.5,
     max=0.5)
 
+bpy.types.Scene.dup_limit_num_duplications = BoolProperty(
+    name="Limit",
+    description="Limit the number of objects to be duplicated",
+    default=False)
+
+bpy.types.Scene.dup_max_duplications = IntProperty(
+    name="Max",
+    description="Maximum number of objects to be duplicated",
+    default=20,
+    min=0)
+
 classes = (
     DUP_DuplicateToCursorOperator,
     DUP_DuplicateToFacesOperator,
     DUP_DuplicateToVerticesOperator,
     DUP_DeleteDuplicatedChildrenOperator,
-    DUP_PT_Panel
+    DuplicationPanel,
+    DuplicationOptionsPanel,
+    DuplicationTargetPanel
 )
 
 register, unregister = bpy.utils.register_classes_factory(classes)
--- dup_panel.py
+++ dup_panel.py
@@ -1,7 +1,7 @@
 import bpy
 
 
-class DUP_PT_Panel(bpy.types.Panel):
+class DuplicationPanel(bpy.types.Panel):
     bl_idname = "DUP_PT_Panel"
     bl_label = "Duplicate selected to:"
     bl_space_type = "VIEW_3D"
@@ -16,22 +16,55 @@ class DUP_PT_Panel(bpy.types.Panel):
         row.operator('object.duplicate_to_vertices', text="Vertices")
         row.operator('object.duplicate_to_cursor', text="Cursor")
 
-        layout.row()
-        layout.prop(context.scene, "dup_duplicate_tree")
-        layout.row()
-        layout.prop(context.scene, "dup_randomise_duplicated")
-        layout.row()
-        layout.prop(context.scene, "dup_random_rotation_factor")
-        layout.row()
-        layout.prop(context.scene, "dup_randomise_displacement")
-        layout.row()
-        layout.prop(context.scene, "dup_random_displacement")
-        layout.row()
-        layout.prop(context.scene, "dup_randomise_scale")
-        layout.row()
-        layout.prop(context.scene, "dup_random_scale")
+
+class DuplicationOptionsPanel(bpy.types.Panel):
+    bl_idname = "Duplication_Options_Panel"
+    bl_label = "Duplication options"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Duplicator"
+
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        row.prop(context.scene, "dup_duplicate_tree")
+
+        row = layout.row()
+        row.label(text='Randomise rotation:')
+        row = layout.row(align=True)
+        row.prop(context.scene, "dup_randomise_duplicated")
+        row.prop(context.scene, "dup_random_rotation_factor")
+
+        row = layout.row()
+        row.label(text='Randomise displacement:')
+        row = layout.row(align=True)
+        row.prop(context.scene, "dup_randomise_displacement")
+        row.prop(context.scene, "dup_random_displacement")
+
+        row = layout.row()
+        row.label(text='Randomise scale:')
+        row = layout.row(align=True)
+        row.prop(context.scene, "dup_randomise_scale")
+        row.prop(context.scene, "dup_random_scale")
+
         layout.row()
         layout.prop(context.scene, "dup_density")
 
+        row = layout.row()
+        row.label(text='Limit duplications:')
+        row = layout.row(align=True)
+        row.prop(context.scene, "dup_limit_num_duplications")
+        row.prop(context.scene, "dup_max_duplications")
+
+
+class DuplicationTargetPanel(bpy.types.Panel):
+    bl_idname = "Duplication_Target_Panel"
+    bl_label = "Duplication Target"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Duplicator"
+
+    def draw(self, context):
+        layout = self.layout
         row = layout.row()
         row.operator('object.delete_duplicated_children', text="Remove duplicated children")
